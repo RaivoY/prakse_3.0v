5acc51ba183311f7bc350a8db9f567e649dc996b
Rungis <raivo.rungis@bertelsmann.de>
2024-12-15 20:20:00 +0200

    first commit

---
+++ database/migrations/2024_12_04_103542_create_form_submissions_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFormSubmissionsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('form_submissions', function (Blueprint $table) {
+            $table->id();
+            $table->string('email');
+            $table->text('comments');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('form_submissions');
+    }
+}
+
